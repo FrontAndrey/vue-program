62ec6b69e2868fdc1d1e0612eba30f75738ffdbe
Andrey <butor.2011@mail.ru>
2022-07-08 15:07:38 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="app">
+        <h1>Страница с постами</h1>
+        <div class="app__btns">
+            <my-button @click="showDialog">Создать пост</my-button>
+            <my-select v-model="selectedSort" :options="sortOptions" />
+        </div>
+
+        <my-dialog v-model:show="dialogVisible">
+            <post-form @create="createPost" />
+        </my-dialog>
+
+        <post-list :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+        <div v-else>Идет загрузка...</div>
+
+    </div>
+</template>
+
+<script>
+import PostList from "./components/PostList.vue";
+import PostForm from "./components/PostForm.vue";
+import MyButton from "./components/UI/MyButton.vue";
+import axios from "axios";
+import MySelect from "./components/UI/MySelect.vue";
+export default {
+    components: {
+        PostList,
+        PostForm,
+        MyButton,
+        MySelect
+    },
+
+    data() {
+        return {
+            posts: [],
+            dialogVisible: false,
+            isPostsLoading: false,
+            selectedSort: '',
+            sortOptions: [
+                { value: 'title', name: 'По названию' },
+                { value: 'body', name: 'По содержимому' }
+            ]
+
+        }
+    },
+    methods: {
+        createPost(post) {
+            this.posts.push(post);
+            this.dialogVisible = false;
+        },
+        removePost(post) {
+            this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog() {
+            this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try {
+                this.isPostsLoading = true;
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                this.posts = response.data;
+            } catch (e) {
+                alert('Ошибка')
+            } finally {
+                this.isPostsLoading = false;
+
+            }
+        }
+    },
+    mounted() {
+        this.fetchPosts();
+    },
+    watch: {
+        selectedSort(newValue) {
+
+        }
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.app {
+    padding: 20px;
+}
+
+.app__btns {
+    margin: 15px 0px;
+    display: flex;
+    justify-content: space-between;
+}
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "my-button"
+}
+</script>
+
+<style scoped>
+.btn {
+    color: teal;
+
+    padding: 10px 15px;
+    background: none;
+    border: 1px solid teal;
+}
+</style>
